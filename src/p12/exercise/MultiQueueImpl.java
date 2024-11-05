176bfc82414252a8a584a3efe0f2d2aceff88acf
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2024-11-05 19:39:22 +0100

    finito nella versione 1.0

--- src/p12/exercise/MultiQueueImpl.java
+++ src/p12/exercise/MultiQueueImpl.java
@@ -1,63 +1,116 @@
 package p12.exercise;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
+    private final Map<Q, Queue<T>> code;
+
+    public MultiQueueImpl() {
+        code=new HashMap<>();
+    }
 
     @Override
     public Set<Q> availableQueues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'availableQueues'");
+        Set<Q> ris=new HashSet<>();
+        for (Q q : code.keySet()) {
+            ris.add(q);
+        }
+        return ris;
     }
 
     @Override
     public void openNewQueue(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'openNewQueue'");
+        if(code.containsKey(queue)) {
+            throw new IllegalArgumentException("Già presente");
+        }
+        Queue<T> a=new LinkedList<>();
+        code.put(queue, a);
     }
 
     @Override
     public boolean isQueueEmpty(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isQueueEmpty'");
+        if(code.containsKey(queue)) {
+            return code.get(queue).isEmpty();
+        } else {
+            throw new IllegalArgumentException("Coda inesistente");
+        }
     }
 
     @Override
     public void enqueue(T elem, Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'enqueue'");
+        if(code.containsKey(queue)) {
+            code.get(queue).add(elem);
+        } else {
+            throw new IllegalArgumentException("La coda non esiste");  
+        }
     }
 
     @Override
     public T dequeue(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dequeue'");
+        if(code.containsKey(queue)) {
+            return code.get(queue).poll();
+        } else {
+            throw new IllegalArgumentException("La Coda non esiste");
+        }
     }
 
     @Override
     public Map<Q, T> dequeueOneFromAllQueues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dequeueOneFromAllQueues'");
+        Map<Q, T> mappa=new HashMap<>();
+        Set<Q> setChiavi=new HashSet<>();
+        setChiavi=code.keySet();
+        for (Q q : setChiavi) {
+            mappa.put(q, code.get(q).poll());
+        }
+        return mappa;
     }
 
     @Override
     public Set<T> allEnqueuedElements() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'allEnqueuedElements'");
+        Set<Q> setChiavi = new HashSet<>();
+        setChiavi=code.keySet();
+        Set<T> elementi=new HashSet<>();
+        for (Q q : setChiavi) {
+            elementi.addAll(code.get(q));
+        }
+        return elementi;
     }
 
     @Override
     public List<T> dequeueAllFromQueue(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dequeueAllFromQueue'");
+        if(code.containsKey(queue)) {
+            List<T> ris=new ArrayList<>();
+            ris.addAll(code.get(queue));
+            code.get(queue).clear();
+            return ris;
+        } else {
+            throw new IllegalArgumentException("la coda non esiste");
+        }
     }
 
     @Override
     public void closeQueueAndReallocate(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'closeQueueAndReallocate'");
+        if(code.containsKey(queue)) {
+            if(code.size()!=1)  {
+                Queue coda=new LinkedList<>();
+                coda=code.get(queue);
+                code.remove(queue);
+                for (Q chiave : code.keySet()) {
+                    code.get(chiave).addAll(coda);
+                }
+            } else {
+                throw new IllegalStateException("non ci sono altre code in cui inserire i valori");
+            }
+        } else {
+            throw new IllegalArgumentException("la coda non esiste");
+        }
     }
 
 }
